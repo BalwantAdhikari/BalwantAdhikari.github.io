9e8b1d63f7174d9be8a481d9cdde37c515bd49af
Balwant Adhikari <balwantadhikari123@gmail.com>
2021-02-11 15:17:31 +0530

    using document.window.width/height for testing

--- games/infiniteJumper/src/main.js
+++ games/infiniteJumper/src/main.js
@@ -13,8 +13,8 @@ console.log(window.mobileAndTabletCheck())
 
 export default new Phaser.Game({
     type: Phaser.AUTO,
-    width: window.mobileAndTabletCheck() ? window.innerWidth : 480,
-    height: window.mobileAndTabletCheck() ? window.innerHeight : 640,
+    width: window.mobileAndTabletCheck() ? document.window.width : 480,
+    height: window.mobileAndTabletCheck() ? document.window.height : 640,
     scene: [Game, GameOver],
     physics: {
         default: 'arcade',
