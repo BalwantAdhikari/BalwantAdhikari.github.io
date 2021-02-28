223e631a97bca81ddad810a67a95be52d0c2ebb3
Balwant Adhikari <balwantadhikari123@gmail.com>
2021-02-28 10:46:35 +0530

    traffic light logic done

--- games/OYOrooms/src/scenes/Game.js
+++ games/OYOrooms/src/scenes/Game.js
@@ -16,7 +16,10 @@ export default class Game extends Phaser.Scene
         this.load.image('right-road', 'assets/object-27.png')
         this.load.image('hr-rule', 'assets/object-32.png')
         this.load.image('cloud', 'assets/object-29.png')
-        this.load.image('traffic-light', 'assets/object-11.png')
+        this.load.image('traffic-light-red', 'assets/object-33.png')
+        this.load.image('traffic-light-green', 'assets/object-34.png')
+
+        this.load.image('hotel', 'assets/object-16.png')
     }
 
     create()
@@ -36,7 +39,11 @@ export default class Game extends Phaser.Scene
         this.cloud1 = this.add.image(0, 0, 'cloud')
         this.cloud2 = this.add.image(0, 0, 'cloud')
         this.cloud3 = this.add.image(0, 0, 'cloud')
-        this.trafficLight = this.add.image(0, 0, 'traffic-light')
+        this.hotel = this.add.image(0, 0, 'hotel')
+        this.hotel.setOrigin(0.5, 1)
+        this.trafficLightRed = this.add.image(0, 0, 'traffic-light-red')
+        this.trafficLightGreen = this.add.image(0, 0, 'traffic-light-green')
+        this.trafficLightGreen.setVisible(false)
         this.bike = this.physics.add.sprite(0, 0, 'bike')
 
         this.aGrid = new AlignGrid({scene:this, rows:16, cols:9})
@@ -70,8 +77,11 @@ export default class Game extends Phaser.Scene
         this.aGrid.placeAtIndex(42, this.cloud3)
         Align.scaleToGameW(this.cloud3, 0.14)
 
-        this.aGrid.placeAtIndex(57, this.trafficLight)
-        Align.scaleToGameW(this.trafficLight, 0.3)
+        this.aGrid.placeAtIndex(57, this.trafficLightRed)
+        Align.scaleToGameW(this.trafficLightRed, 0.3)
+
+        this.aGrid.placeAtIndex(65, this.hotel)
+        Align.scaleToGameW(this.hotel, 0.1)
 
         // set bounds 
         this.physics.world.setBounds(this.scale.width/6.4, 0, this.scale.width/1.43, this.scale.height);
@@ -129,5 +139,68 @@ export default class Game extends Phaser.Scene
             this.bike.x += 2
         }
 
+        if(this.count < 350)
+        {
+            // scale and move hotel
+            this.hotel.x -= 0.05
+            this.hotel.y += 0.05
+    
+            Align.scaleToGameW(this.hotel, 0.2 + this.count/7000)
+    
+    
+            // scale and move traffic light
+            this.trafficLightRed.x -= 0.1  
+            this.trafficLightRed.y += 0.3
+    
+            Align.scaleToGameW(this.trafficLightRed, 0.3 + this.count/1000)
+        }
+
+        // if(this.count > 350 && this.count < 450)
+        // {
+        //     this.trafficLightRed.setVisible(false)
+
+        //     this.trafficLightGreen.setVisible(true)
+        //     // this.trafficLightGreen.x = this.trafficLightRed.x
+        //     // this.trafficLightGreen.y = this.trafficLightRed.y
+
+
+        // }
+
+        if(this.count == 450)
+        {
+            this.trafficLightRed.setVisible(false)
+            this.trafficLightGreen.setVisible(true)
+
+            this.trafficLightGreen.x = this.trafficLightRed.x
+            this.trafficLightGreen.y = this.trafficLightRed.y
+
+            Align.scaleToGameW(this.trafficLightGreen, 0.3 + (this.count - 100)/1000)
+
+        }
+
+        if(this.count > 450 && this.count < 700)
+        {
+            // scale and move hotel
+            this.hotel.x -= 0.05
+            this.hotel.y += 0.05
+    
+            Align.scaleToGameW(this.hotel, 0.2 + (this.count - 100)/7000)
+    
+            if(this.count > 550)
+            {
+                this.trafficLightGreen.setVisible(false)
+            }
+    
+            // scale and move traffic light
+            this.trafficLightGreen.x -= 0.1  
+            this.trafficLightGreen.y += 0.3
+    
+            Align.scaleToGameW(this.trafficLightGreen, 0.3 + (this.count - 100)/1000)
+            
+        }
+
+
+
     }
+
 }
