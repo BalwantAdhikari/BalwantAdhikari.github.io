3ca9432afd3899caa934e31313001af451136f2d
Balwant Adhikari <balwantadhikari123@gmail.com>
2021-03-11 16:59:13 +0530

    added scaling through align.js

--- games/MMT/src/scenes/Game.js
+++ games/MMT/src/scenes/Game.js
@@ -1,5 +1,7 @@
 import Phaser from '../lib/Phaser.js'
 
+import Align from '../lib/util/align.js'
+
 export default class Game extends Phaser.Scene{
 
     count = 0
@@ -19,29 +21,36 @@ export default class Game extends Phaser.Scene{
         this.load.image('wheel', 'assets/wheel-copy3.png')
         this.load.image('stopper', 'assets/Wheel_Stoper.png')
         this.load.image('coin', 'assets/coin.png')
-        this.load.audio('tickSound', 'assets/tick.mp3')
+        this.load.audio('tickSound', 'assets/ding.mp3')
         this.load.audio('applaude', 'assets/applaude.mp3')
     }
 
     create()
     {
         this.tickSound = this.sound.add('tickSound', {
-            volume: 0.1
+            volume: 0.3
         })
 
         this.applaudeSound = this.sound.add('applaude', {
-            volume: 0.1
+            volume: 0.2
         })
-        this.add.image(this.scale.width/2, this.scale.height/2, 'background').setScale(1)
-        this.wheelReflection = this.add.image(this.scale.width/2, this.scale.height/2, 'wheel-reflection').setScale(0.5)
-        this.add.image(this.scale.width/2, this.scale.height/11, 'header-text').setScale(0.45)
-        this.playButton = this.add.image(this.scale.width/2, this.scale.height - (this.scale.height/11), 'play-button').setScale(0.4)
-        this.rightCoin = this.add.image(this.scale.width/4 + this.scale.width/10, this.scale.height/2 + this.scale.height/5, 'coin').setScale(0.3).setOrigin(0.5)
+        this.background = this.add.image(this.scale.width/2, this.scale.height/2, 'background').setScale(1)
+        this.wheelReflection = this.add.image(this.scale.width/2, this.scale.height/2, 'wheel-reflection')
+        Align.scaleToGameW(this.wheelReflection, 2)
+        this.headerText = this.add.image(this.scale.width/2, this.scale.height/11, 'header-text')
+        Align.scaleToGameW(this.headerText, 1.5)
+        this.playButton = this.add.image(this.scale.width/2, this.scale.height - (this.scale.height/11), 'play-button')
+        Align.scaleToGameW(this.playButton, 1.3)
+        this.rightCoin = this.add.image(this.scale.width/4 + this.scale.width/10, this.scale.height/2 + this.scale.height/5, 'coin').setOrigin(0.5)
+        Align.scaleToGameW(this.rightCoin, 0.3)
         this.rightCoin.setVisible(false)
-        this.leftCoin = this.add.image(this.scale.width* 3/4 - this.scale.width/10, this.scale.height/2 + this.scale.height/5, 'coin').setScale(0.3).setOrigin(0.5).setFlipX(true)
+        this.leftCoin = this.add.image(this.scale.width* 3/4 - this.scale.width/10, this.scale.height/2 + this.scale.height/5, 'coin').setOrigin(0.5).setFlipX(true)
+        Align.scaleToGameW(this.leftCoin, 0.3)
         this.leftCoin.setVisible(false)
-        this.wheel = this.physics.add.image(this.scale.width/2, this.scale.height/2, 'wheel').setScale(0.35).setOrigin(0.5)
-        this.stoper = this.physics.add.image(this.wheel.x, this.wheel.y - this.wheel.height * 0.35/2, 'stopper').setScale(0.3).setOrigin(0.5, 0.1)
+        this.wheel = this.physics.add.image(this.scale.width/2, this.scale.height/2, 'wheel').setOrigin(0.5)
+        Align.scaleToGameW(this.wheel, 0.9)
+        this.stoper = this.physics.add.image(this.scale.width/2, this.scale.height/2 - this.wheel.height * this.wheel.scale/2, 'stopper').setOrigin(0.5, 0.1)
+        Align.scaleToGameW(this.stoper, 1)
         this.displayPrize = this.add.graphics()
         this.displayPrize.fillRect(0, 0, this.scale.width, this.scale.height);
 
@@ -66,11 +75,6 @@ export default class Game extends Phaser.Scene{
  
         // so the wheel does not fall in y axis
         this.physics.world.gravity.y = 0
-
-        // testing
-        // const circle = new Phaser.Geom.Circle(this.wheel.x, this.wheel.y, this.wheel.height * 0.35/2);
-        // this.group = this.add.group({ key: 'stopper', frame: [0, 2, 5], repeat: 1});
-        // Phaser.Actions.PlaceOnCircle(this.group.getChildren(), circle);
 
         this.playButton.setInteractive().on('pointerdown', function(pointer, localX, localY, event){
             if(this.canSpin)
@@ -123,7 +127,7 @@ export default class Game extends Phaser.Scene{
                             this.applaudeSound.play()
                             this.leftCoin.setVisible(true)
                             this.rightCoin.setVisible(true)
-                            this.displayPrize.fillStyle(0x000000, 0.6)
+                            this.displayPrize.fillStyle(0x000000, 0.7)
                             // displaying prize text
                             this.prizeText.setText(this.slicePrizes[prize]);
                         }
