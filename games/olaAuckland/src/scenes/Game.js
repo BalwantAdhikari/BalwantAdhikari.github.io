33eca18de8a8ef7974344aad13c7d861931bc360
Balwant Adhikari <balwantadhikari123@gmail.com>
2021-04-25 18:07:49 +0530

    items placed in room

--- games/olaAuckland/src/scenes/Game.js
+++ games/olaAuckland/src/scenes/Game.js
@@ -19,25 +19,95 @@ export default class Game extends Phaser.Scene {
     {
         this.background = this.add.image(this.scale.width/2, this.scale.height/2, "background")
         Align.scaleToGameW(this.background, 1)
+        
+        // this.diary = this.add.image(0, 0, "diary")
+        // Align.scaleToGameW(this.diary, 0.14)
+        this.diaryGlow = this.add.image(0, 0, "diaryGlow")
+        Align.scaleToGameW(this.diaryGlow, 0.14)
+        // this.headphones = this.add.image(0, 0, "headphones")
+        // Align.scaleToGameW(this.headphones, 0.12)
+        this.headphonesGlow = this.add.image(0, 0, "headphonesGlow")
+        Align.scaleToGameW(this.headphonesGlow, 0.12)
         this.keyHolder = this.add.image(0, 0, "keyHolder").setOrigin(0.5, 0)
         Align.scaleToGameW(this.keyHolder, 0.2)
-        this.keys = this.add.image(0, 0, "keys").setOrigin(0.1, 0)
-        Align.scaleToGameW(this.keys, 0.08)
-        this.mask = this.add.image(0, 0, "mask").setOrigin(1, 0)
-        Align.scaleToGameW(this.mask, 0.08)
+        // this.keys = this.add.image(0, 0, "keys").setOrigin(0.1, 0)
+        // Align.scaleToGameW(this.keys, 0.06)
+        this.keysGlow = this.add.image(0, 0, "keysGlow").setOrigin(0.1, 0)
+        Align.scaleToGameW(this.keysGlow, 0.06)
+        // this.laptop = this.add.image(0, 0, "laptop")
+        // Align.scaleToGameW(this.laptop, 0.19)
+        this.laptopGlow = this.add.image(0, 0, "laptopGlow")
+        Align.scaleToGameW(this.laptopGlow, 0.19)
+        // this.lunch = this.add.image(0, 0, "lunch")
+        // Align.scaleToGameW(this.lunch, 0.12)
+        this.lunchGlow = this.add.image(0, 0, "lunchGlow")
+        Align.scaleToGameW(this.lunchGlow, 0.12)
+        // this.mask = this.add.image(0, 0, "mask").setOrigin(1, 0)
+        // Align.scaleToGameW(this.mask, 0.06)
         this.maskGlow = this.add.image(0, 0, "maskGlow").setOrigin(1, 0)
-        Align.scaleToGameW(this.maskGlow, 0.1)
-        this.sanitizer = this.add.image(0, 0, "sanitizer").setOrigin(0.5, 1)
-        Align.scaleToGameW(this.sanitizer, 0.08)
+        Align.scaleToGameW(this.maskGlow, 0.06)
+        // this.phone = this.add.image(0, 0, "phone")
+        // Align.scaleToGameW(this.phone, 0.12)
+        this.phoneGlow = this.add.image(0, 0, "phoneGlow")
+        Align.scaleToGameW(this.phoneGlow, 0.12)
+        // this.sanitizer = this.add.image(0, 0, "sanitizer").setOrigin(0.5, 1)
+        // Align.scaleToGameW(this.sanitizer, 0.06)
         this.sanitizerGlow = this.add.image(0, 0, "sanitizerGlow").setOrigin(0.5, 0.35)
-        Align.scaleToGameW(this.sanitizerGlow, 0.08)
+        Align.scaleToGameW(this.sanitizerGlow, 0.06)
+        // this.shades = this.add.image(0, 0, "shades")
+        // Align.scaleToGameW(this.shades, 0.1)
+        this.shadesGlow = this.add.image(0, 0, "shadesGlow")
+        Align.scaleToGameW(this.shadesGlow, 0.1)
+        // this.wallet = this.add.image(0, 0, "wallet")
+        // Align.scaleToGameW(this.wallet, 0.12)
+        this.walletGlow = this.add.image(0, 0, "walletGlow")
+        Align.scaleToGameW(this.walletGlow, 0.12)
 
         this.aGrid = new AlignGrid({scene:this, rows:16, cols:9})
         // this.aGrid.showNumbers()
 
         this.aGrid.placeAtIndex(47, this.keyHolder)
-        this.aGrid.placeAtIndex(46, this.keys)
-        this.aGrid.placeAtIndex(48, this.mask)
-        this.aGrid.placeAtIndex(55, this.sanitizerGlow)
+        this.aGrid.placeAtIndex(46, this.keysGlow)
+        this.aGrid.placeAtIndex(48, this.maskGlow)
+
+        // screen width/height
+        this.width = this.scale.width
+        this.height = this.scale.height
+        // bgImage width/height
+        this.bgWidth = this.background.scaleX * this.background.width
+        this.bgHeight = this.background.scaleX * this.background.width
+
+        // sanitizer
+        this.sanitizerGlow.x = this.bgWidth / 6.5
+        this.sanitizerGlow.y = (this.bgHeight / 3.15) + ((this.height - this.bgHeight) / 2)
+
+        // shades
+        this.shadesGlow.x = this.bgWidth - this.bgWidth / 7
+        this.shadesGlow.y = (this.bgHeight / 2 + this.bgHeight / 6.5) + ((this.height - this.bgHeight) / 2)
+
+        // lunch
+        this.lunchGlow.x = this.bgWidth - this.bgWidth / 4
+        this.lunchGlow.y = (this.bgHeight / 2 + this.bgHeight / 2.3)  + ((this.height - this.bgHeight) / 2)
+
+        // diary
+        this.diaryGlow.x = this.bgWidth - this.bgWidth / 7
+        this.diaryGlow.y = (this.bgHeight / 2 + this.bgHeight / 2.15) + ((this.height - this.bgHeight) / 2)
+
+        // headphones
+        this.headphonesGlow.x = this.bgWidth - this.bgWidth / 7
+        this.headphonesGlow.y = (this.bgHeight / 2 + this.bgHeight / 2.3) + ((this.height - this.bgHeight) / 2)
+
+        // wallet
+        this.walletGlow.x = this.bgWidth / 3.5
+        this.walletGlow.y = (this.bgHeight / 2 + this.bgHeight / 2.6) + ((this.height - this.bgHeight) / 2)
+
+        // phone
+        this.phoneGlow.x = this.bgWidth / 5.4
+        this.phoneGlow.y = (this.bgHeight / 2 + this.bgHeight / 4.4) + ((this.height - this.bgHeight) / 2)
+
+        // laptop
+        this.laptopGlow.x = this.bgWidth / 3.2
+        this.laptopGlow.y = (this.bgHeight / 2 + this.bgHeight / 4.6) + ((this.height - this.bgHeight) / 2)
+
     }
 }
--- games/olaAuckland/src/scenes/Preload.js
+++ games/olaAuckland/src/scenes/Preload.js
@@ -16,12 +16,27 @@ export default class Preload extends Phaser.Scene{
         this.counter = 0;
 
         this.load.image('background', 'assets/BG-13.jpg')
+        this.load.image('diary', 'assets/Diary.png')
+        this.load.image('diaryGlow', 'assets/DiaryGlow.png')
+        this.load.image('headphones', 'assets/Headphones.png')
+        this.load.image('headphonesGlow', 'assets/HeadphonesGlow.png')
         this.load.image('keyHolder', 'assets/Key-Holder.png')
-        this.load.image('keys', 'assets/KeysGlow.png')
+        this.load.image('keys', 'assets/Keys.png')
+        this.load.image('keysGlow', 'assets/KeysGlow.png')
+        this.load.image('laptop', 'assets/Laptop.png')
+        this.load.image('laptopGlow', 'assets/LaptopGlow.png')
+        this.load.image('lunch', 'assets/Lunch.png')
+        this.load.image('lunchGlow', 'assets/LunchGlow.png')
         this.load.image('mask', 'assets/Mask.png')
         this.load.image('maskGlow', 'assets/MaskGlow.png')
+        this.load.image('phone', 'assets/Phone.png')
+        this.load.image('phoneGlow', 'assets/PhoneGlow.png')
         this.load.image('sanitizer', 'assets/Sanitizer.png')
         this.load.image('sanitizerGlow', 'assets/SanitizerGlow.png')
+        this.load.image('shades', 'assets/Shades.png')
+        this.load.image('shadesGlow', 'assets/ShadesGlow.png')
+        this.load.image('wallet', 'assets/Wallet.png')
+        this.load.image('walletGlow', 'assets/WalletGlow.png')
     }
 
     create()
