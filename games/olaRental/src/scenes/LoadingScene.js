cb78cc08a3dfa233da66e5735a22b93bca59b02c
Balwant Adhikari <balwantadhikari123@gmail.com>
2021-08-06 17:34:49 +0530

    image quality testing 6

--- games/olaRental/src/scenes/LoadingScene.js
+++ games/olaRental/src/scenes/LoadingScene.js
@@ -27,7 +27,7 @@ export default class LoadingScene extends Phaser.Scene
         this.load.image('loading-car4miniSelected', 'assets/frame1-car4miniSelected.webp')
         this.load.image('loading-text', 'assets/frame1-text.webp')
         this.load.image('loading-sun', 'assets/frame1-sun.webp')
-        this.load.image('loading-logo', 'assets/frame1-logo.webp')
+        this.load.image('loading-logo', 'assets/frame1-logo@3x.png')
         this.load.image('loading-startButton', 'assets/frame1-startButton.webp')
 
         // frame 2
@@ -43,8 +43,7 @@ export default class LoadingScene extends Phaser.Scene
     {
         // background
         this.loadingBackground = this.add.image(this.scale.width/2, this.scale.height/2, 'loading-background')
-        // Align.scaleToGameW(this.loadingBackground, 1)
-        this.loadingBackground.scale = window.devicePixelRatio
+        Align.scaleToGameW(this.loadingBackground, 1)
 
         // car
         // this.loadingcar1 = this.add.image(this.scale.width/2, this.scale.height/2, 'loading-car1')
@@ -88,7 +87,7 @@ export default class LoadingScene extends Phaser.Scene
 
         // sun
         this.loadingsun = this.add.image(this.scale.width/2, this.scale.height/2, 'loading-sun')
-        // Align.scaleToGameW(this.loadingsun, 1)
+        Align.scaleToGameW(this.loadingsun, 1)
 
 
         // text
@@ -97,8 +96,7 @@ export default class LoadingScene extends Phaser.Scene
 
         // logo
         this.loadinglogo = this.add.image(this.scale.width/2, this.scale.height/2, 'loading-logo')
-        // Align.scaleToGameW(this.loadinglogo, 0.25)
-        this.loadinglogo.scale = window.devicePixelRatio
+        Align.scaleToGameW(this.loadinglogo, 0.25)
 
         // start Button
         this.loadingstartButton = this.add.image(this.scale.width/2, this.scale.height/2, 'loading-startButton')
