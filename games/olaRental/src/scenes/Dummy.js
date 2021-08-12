23a6bf3922d3d9c21c008398a92db3df42b3b83f
Balwant Adhikari <balwantadhikari123@gmail.com>
2021-08-12 12:34:11 +0530

    added crash animation1

--- games/olaRental/src/scenes/Dummy.js
+++ games/olaRental/src/scenes/Dummy.js
@@ -22,8 +22,35 @@ export default class Dummy extends Phaser.Scene
 
     create()
     {
-        this.crashSign = this.add.image(this.car.x, this.car.y, `game-msg${this.coinName.charAt(this.coinName.length - 1)}`)
+        this.animationY = this.car.y - ((this.car.height * this.car.scale))
+        this.crashAnim1 = this.add.image(this.car.x, this.animationY, 'game-crashAnim1')
+        Align.scaleToGameW(this.crashAnim1, 0.5)
+        this.crashAnim2 = this.add.image(this.car.x, this.animationY, 'game-crashAnim2')
+        Align.scaleToGameW(this.crashAnim2, 0.3)
+        this.crashAnim3 = this.add.image(this.car.x, this.animationY, 'game-crashAnim3')
+        Align.scaleToGameW(this.crashAnim3, 0.3)
+
+        this.crashSign = this.add.image(this.car.x, this.animationY, `game-msg${this.coinName.charAt(this.coinName.length - 1)}`)
         Align.scaleToGameW(this.crashSign, 0.5)
+
+        this.tweens.add({
+            targets: this.crashAnim1,
+            angle: 90,
+            duration: 1500,
+        })
+        this.tweens.add({
+            targets: this.crashAnim2,
+            scale: 0.5,
+            angle: 90,  
+            duration: 1500,
+        })
+        this.tweens.add({
+            targets: this.crashAnim3,
+            scale: 0.5,
+            angle: -180,
+            duration: 1500,
+        })
+
 
         this.time.delayedCall(1500, () => {
             this.coin.destroy()
